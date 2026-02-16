b80400cb71af8035a9903010ebc636c81280d094
Narvin Sachaniya <githubnarvin@gmail.com>
2026-02-16 21:41:51 +0530

    Update db.js

--- crm_backend/config/db.js
+++ crm_backend/config/db.js
@@ -3,11 +3,7 @@ require("dotenv").config();
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-      dbName: "openhubDB"
-    });
+    await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected...");
   } catch (error) {
     console.error("MongoDB Connection Failed", error);
